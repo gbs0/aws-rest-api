c79bea35a1b12bdae174a2dde5d7568a532d1bb0
gabriel schiavo <gabriel@lewagon.org>
2020-02-11 13:10:57 -0300

    fix parsing on JSON

--- src/creationGet.js
+++ src/creationGet.js
@@ -3,32 +3,17 @@ const AWS = require('aws-sdk');
   
 exports.handler = async (event, context) => {
   const docClient = new AWS.DynamoDB.DocumentClient();
-  console.log(event);
 
   let responseBody = "";
   let statusCode = 0;
 
-  const { address, birth, cpf, email, id, income, loan, maritial, name, user_id } = JSON.parse(event.body);
-
   const params = {
-    TableName: "Creations",
-    Item: {
-      address: address,
-      birth: birth,
-      cpf: cpf,
-      email: email,
-      id: id,
-      income: income,
-      loan: loan,
-      maritial: maritial,
-      name: name,
-      user_id: user_id
-    }
+    TableName: "Creations"
   };
 
   try {
     const data = await docClient.put(params).promise();
-    responseBody = JSON.stringify(data);
+    responseBody = JSON.stringify(data.Items);
     statusCode = 201;
   } catch(err) {
     responseBody = `Unable to put : ${err}`;
