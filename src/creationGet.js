01377f6bdf84b216ebae3876bc0c3621fd270dbf
gabriel schiavo <gabriel@lewagon.org>
2020-02-11 13:17:25 -0300

    fix file indentation

--- src/creationGet.js
+++ src/creationGet.js
@@ -1,32 +1,32 @@
-	'use strict';
-	const AWS = require('aws-sdk');
-	  
-	exports.handler = async (event, context) => {
-	  const docClient = new AWS.DynamoDB.DocumentClient();
+'use strict';
+const AWS = require('aws-sdk');
+  
+exports.handler = async (event, context) => {
+  const docClient = new AWS.DynamoDB.DocumentClient();
 
-	  let responseBody = "";
-	  let statusCode = 0;
+  let responseBody = "";
+  let statusCode = 0;
 
-	  const params = {
-	    TableName: "Creations"
-	  };
+  const params = {
+    TableName: "Creations"
+  };
 
-	  try {
-	    const data = await docClient.put(params).promise();
-	    responseBody = JSON.stringify(data.Items);
-	    statusCode = 201;
-	  } catch(err) {
-	    responseBody = `Unable to put : ${err}`;
-	    statusCode = 403;
-	  }
+  try {
+    const data = await docClient.put(params).promise();
+    responseBody = JSON.stringify(data.Items);
+    statusCode = 201;
+  } catch(err) {
+    responseBody = `Unable to put : ${err}`;
+    statusCode = 403;
+  }
 
-	  const response = {
-	    statusCode: statusCode,
-	    headers: {
-	      "Content-Type": "application/json"
-	    },
-	    body: responseBody
-	  };
+  const response = {
+    statusCode: statusCode,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: responseBody
+  };
 
-	  return response;
-	};
+  return response;
+};
